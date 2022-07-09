ec214616bc373ea7354d6666868e377fc898e9da
DelZee <104604081+DelZee@users.noreply.github.com>
2022-07-08 22:43:06 -0700

    Update Covid_portfolio_project_sqlscript.sql

--- Covid_portfolio_project_sqlscript.sql
+++ Covid_portfolio_project_sqlscript.sql
@@ -147,5 +147,92 @@ From CovidDeaths dea
 		--order by 2,3
 		
 		
+				--United States Covid breakdown by country and Per 100 k population
+
+DECLARE		@PopulationLimit Int
+SET			@PopulationLimit = 20000
+       
+
+SELECT		TOP 25 
+			cif.county AS County, 
+			cif.[State],
+			FORMAT(Cd.cases, 'N0') AS 'Total Cases', 
+			FORMAT(cif.Population, 'N0') as Population,
+			FORMAT((100000*(Cd.cases*1.0/cif.Population)),'N0') as 'Per 100K Cases'
+
+FROM		dbo.COVIDCountyData as Cd
+JOIN		dbo.CountyInfo as cif on cif.FIPS = Cd.fips
+WHERE	Cd.[date] = (select MAX(ccd.[date]) FROM dbo.COVIDCountyData as ccd) 
+and		cif.Population >= @PopulationLimit
+
+Order by	 ((100000*(Cd.cases*1.0/cif.Population))) DESC
+
+
+		---Creating Stored Procedure for New Deaths and New Cases--
+
+CREATE PROCEDURE UpdateStateDaily As
+Update  s
+Set		NewCases = s.cases - IsNull(sp.cases, 0)
+
+
+From    COVIDStateData As s
+Left Join COVIDStateData As sp 
+on      sp.FIPS = s.FIPS and sp.date = DATEADD(DAY, -1, s.date)
+
+Exec    UpdateStateDaily
+
+Drop Procedure UpdateStateDaily
+
+
+-- Creating Subqueries---
+
+Select		s.State
+		,FORMAT(sd.NewCases, 'N0') as [Max New Cases]
+		,CONVERT(Varchar, Cast(Date as Date), 101) as Date
+		,DATENAME(WEEKDAY, sd.Date) as [Day of Week]
+		,FORMAT((sd.NewCases * 1.0/s.population)*100000, 'N1') as 'Per 100k Rate'
+FROM		(Select 		fips
+							,date
+							,NewCases
+							,ROW_NUMBER() OVER(PARTITION by fips Order by NewCases DESC, date Desc) as RowNumber
+				From	[dbo].[COVIDStateData]) as sd
+				Join	[dbo].[StateInfo] as s on s.FIPS = sd.fips
+				Where			sd.RowNumber = 1
+				Order by		(sd.NewCases * 1.0/s.Population)*100000 DESC
+
+		---- Query for per capita number of cases that are most recent (7 days) by state----
+
+		Select s.State
+	  ,FORMAT(CAST(s.population as numeric), '#,#') as Population
+	  ,FORMAT(sd.[Cases Past Week], 'N0') as [Cases Past Week]
+	  ,FORMAT((sd.[Cases Past Week]* 1.0/s.population)*100000, 'N1') as [Per 100k Rate]
+	  ,RANK() Over( Order by (sd.[Cases Past Week]* 1.0/s.population*100000) Desc) As [Rank]
+From	(Select		Fips
+					,Sum(NewCases) as [Cases Past Week]
+		From		COVIDStateData
+	    where		date > (select Max(DateAdd(DD,-7,date )) from [dbo].[COVIDStateData])
+		Group by	FIPS) As SD
+Join	[dbo].[StateInfo] as s on s.fips = sd.fips
+Group by s.state,s.population, sd.[Cases Past Week]
+Order by  State  Asc
+
+         ---- Query for per capita number of cases that are most recent (7 days) by state---
+
+		 Select s.State
+	  ,FORMAT(CAST(s.population as numeric), '#,#') as Population
+	  ,FORMAT(sd.[Deaths Past Week], 'N0') as [Deaths Past Week]
+	  ,FORMAT((sd.[Deaths Past Week]* 1.0/s.population)*100000, 'N1') as [Per 100k Rate]
+	  ,RANK() Over( Order by (sd.[Deaths Past Week]* 1.0/s.population*100000) Desc) As [Rank]
+From	(Select		Fips
+					,Sum(NewDeaths) as [Deaths Past Week]
+		From		COVIDStateData
+	    where		date > (select Max(DateAdd(DD,-7,date )) from [dbo].[COVIDStateData])
+		Group by	FIPS) As SD
+Join	[dbo].[StateInfo] as s on s.fips = sd.fips
+Group by s.state,s.population, sd.[Deaths Past Week]
+Order by  sd.[Deaths Past Week]* 1.0/s.population*100000  DESC
+
+		
+		
 		
 		
